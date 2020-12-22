b6982e538517357d8e04d554933d0681d46faf31
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-12-21 22:39:50 -0300

    View alunos

--- app/src/main/java/alura/com/br/MainActivity.java
+++ app/src/main/java/alura/com/br/MainActivity.java
@@ -2,8 +2,13 @@ package alura.com.br;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +16,15 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList(
+              "Raphael", "Tosco", "Mais um"
+        ));
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
