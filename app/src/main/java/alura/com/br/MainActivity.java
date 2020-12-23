d0842069af8cfb5ea5e3777440967dd557bbf44e
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-12-22 22:29:55 -0300

    Alterações na listview

--- app/src/main/java/alura/com/br/MainActivity.java
+++ app/src/main/java/alura/com/br/MainActivity.java
@@ -2,7 +2,8 @@ package alura.com.br;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
@@ -17,14 +18,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(Arrays.asList(
-              "Raphael", "Tosco", "Mais um"
+              "Raphael", "Tosco", "Mais um","Maria", "Ana"
         ));
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno = findViewById(R.id.textView2);
-        TextView terceiroAluno = findViewById(R.id.textView3);
-
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                alunos));
     }
 }
