4b5904018e7ca473e111108530f12047693d7c3c
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-01-03 22:19:48 -0300

    Inclusão de ícone

--- app/src/main/java/alura/com/br/MainActivity.java
+++ app/src/main/java/alura/com/br/MainActivity.java
@@ -1,22 +1,23 @@
 package alura.com.br;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList(
               "Raphael", "Tosco", "Mais um","Maria", "Ana"
         ));
