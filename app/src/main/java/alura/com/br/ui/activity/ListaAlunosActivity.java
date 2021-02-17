deb2497ba19a3dde0374e211b51e044a3d452750
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-02-16 23:41:21 -0300

    Finalização de viewadapter personalizado

--- app/src/main/java/alura/com/br/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/alura/com/br/ui/activity/ListaAlunosActivity.java
@@ -3,13 +3,9 @@ package alura.com.br.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -18,12 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import alura.com.br.R;
 import alura.com.br.dao.AlunoDao;
 import alura.com.br.model.Aluno;
+import alura.com.br.ui.activity.adapter.ListaAlunosAdapter;
 
 import static alura.com.br.ui.activity.ConstantesActivities.CHAVE_ALUNO;
 
@@ -31,7 +25,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     public static final String TITULO_APPBAR = "Lista de alunos";
     private final AlunoDao dao = new AlunoDao();
-    private ArrayAdapter<Aluno> adapter;
+    private ListaAlunosAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -122,35 +116,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
     }
 
     private void configuraAdapter(ListView listaDeAlunos) {
-        adapter = new ArrayAdapter<>(this,
-                //android.R.layout.simple_list_item_1);
-                R.layout.item_aluno);
-        listaDeAlunos.setAdapter(new BaseAdapter() {
-
-            private final List<Aluno> alunos = new ArrayList<>();
-
-            @Override
-            public int getCount() {
-                return alunos.size();
-            }
-
-            @Override
-            public Aluno getItem(int position) {
-                return alunos.get(position);
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return alunos.get(position).getId();
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View viewCriada = LayoutInflater
-                        .from(ListaAlunosActivity.this)
-                        .inflate(R.layout.item_aluno,parent);
-                return viewCriada;
-            }
-        });
+        adapter = new ListaAlunosAdapter(this);
+        listaDeAlunos.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/alura/com/br/ui/activity/adapter/ListaAlunosAdapter.java
@@ -0,0 +1,67 @@
+package alura.com.br.ui.activity.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import alura.com.br.R;
+import alura.com.br.model.Aluno;
+
+public class ListaAlunosAdapter  extends BaseAdapter {
+
+    private final List<Aluno> alunos = new ArrayList<>();
+    private Context context;
+
+    public ListaAlunosAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return alunos.size();
+    }
+
+    @Override
+    public Aluno getItem(int position) {
+        return alunos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return alunos.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View viewCriada = LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_aluno, parent,false);
+        Aluno alunoDevolvido = alunos.get(position);
+        TextView nome = viewCriada.findViewById(R.id.item_aluno_nome);
+        nome.setText(alunoDevolvido.getNome());
+        TextView telefone = viewCriada.findViewById(R.id.item_aluno_telefone);
+        telefone.setText(alunoDevolvido.getTelefone());
+        return viewCriada;
+    }
+
+
+    public void clear() {
+        alunos.clear();
+    }
+
+
+    public void addAll(List<Aluno> alunos) {
+        this.alunos.addAll(alunos);
+    }
+
+
+    public void remove(Aluno aluno) {
+        alunos.remove(aluno);
+    }
+}
