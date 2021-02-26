ff8a3fb1e32eeda57ec3abc4638d0aebeb6895c9
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-02-26 01:15:57 -0300

    Inclusão da alertDialog na remoção de um item da lista

--- app/src/main/java/alura/com/br/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/alura/com/br/ui/activity/ListaAlunosActivity.java
@@ -1,6 +1,7 @@
 package alura.com.br.ui.activity;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -35,12 +36,6 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
         configuraLista();
-        new AlertDialog.Builder(this)
-        .setTitle("Removendo aluno")
-        .setMessage("Tem certeza que quer remover o aluno?")
-        .setPositiveButton("Sim",null)
-        .setNegativeButton("Não",null)
-        .show();
     }
 
     @Override
@@ -54,14 +49,28 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         int itemId = item.getItemId();
         if (itemId == R.id.activity_lista_alunos_menu_remover) {
-            AdapterView.AdapterContextMenuInfo menuInfo =
-                    (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-
-            Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
-            remove(alunoEscolhido);
+            confirmaRemocao(item);
         }
 
         return super.onContextItemSelected(item);
+    }
+
+    private void confirmaRemocao(@NonNull MenuItem item) {
+        new AlertDialog.Builder(this)
+                .setTitle("Removendo aluno")
+                .setMessage("Tem certeza que quer remover o aluno?")
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AdapterView.AdapterContextMenuInfo menuInfo =
+                                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+                        Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
+                        remove(alunoEscolhido);
+                    }
+                })
+                .setNegativeButton("Não",null)
+                .show();
     }
 
     private void configuraFabNovoAluno() {
